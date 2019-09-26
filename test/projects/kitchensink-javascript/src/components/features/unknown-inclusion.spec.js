1b79272f6df89aea7130190dead2236483652dbe
Dean Shub <deanshub@gmail.com>
2019-09-26 16:21:54 +0300

    removing unhandled tests

--- test/projects/kitchensink-javascript/src/components/features/unknown-inclusion.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UnknownInclusion from './unknown-inclusion';
-
-it('should pass', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UnknownInclusion />, div);
-});
--- test/projects/kitchensink-typescript/src/components/features/unknown-inclusion.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UnkownInclusion from './unknown-inclusion';
-
-it('should pass', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UnkownInclusion />, div);
-});
