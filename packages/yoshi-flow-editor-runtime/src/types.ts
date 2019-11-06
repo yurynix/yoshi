ef99a97cb42531250d379351f0a35409f0498949
Ran Yitzhaki <ranyitz@gmail.com>
2019-11-06 18:47:50 +0200

    remove unused types

--- packages/yoshi-flow-editor-runtime/src/types.ts
+++
@@ -1,9 +0,0 @@
-// Names of properties in T with types that include undefined
-type OptionalPropertyNames<T> = {
-  [K in keyof T]: undefined extends T[K] ? K : never;
-}[keyof T];
-
-// Common properties from L and R with undefined in R[K] replaced by type in L[K]
-type SpreadProperties<L, R, K extends keyof L & keyof R> = {
-  [P in K]: L[P] | Exclude<R[P], undefined>;
-};
