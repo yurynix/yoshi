196612cb3f15d4220e55103ed11d0d06706f2a0d
Ronen Amiel <ronen.amiel@gmail.com>
2019-11-16 13:49:02 +0200

    wip

--- test/cases/json-inclusion/json-inclusion.test.js
+++ test/cases/json-inclusion/json-inclusion.test.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { matchCSS } = require('../../utils');
 const Scripts = require('../../scripts');
 
 const scripts = new Scripts({
@@ -11,12 +10,8 @@ it.each(['serve', 'start'])('css inclusion %s', async command => {
   await scripts[command](async () => {
     await page.goto('http://localhost:3000');
 
-    const className = await page.$eval('#css-inclusion', elm =>
-      elm.getAttribute('class'),
-    );
+    const result = await page.$eval('#json-inclusion', elm => elm.textContent);
 
-    await matchCSS('app', page, [
-      new RegExp(`.${className}{background:#ccc;color:#000;*}`),
-    ]);
+    expect(result).toBe('This is an abstract.');
   });
 });
