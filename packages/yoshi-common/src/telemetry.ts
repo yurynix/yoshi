74f13a4a221fda133baee2064aefb188899a5b5e
Ronen Amiel <ronen.amiel@gmail.com>
2019-11-26 13:05:14 +0200

    Telemetry (#1722)

---
+++ packages/yoshi-common/src/telemetry.ts
@@ -0,0 +1,34 @@
+import Insight from 'insight';
+import config from 'yoshi-config';
+import semver from 'semver';
+import { isTypescriptProject as checkIsTypescriptProject } from 'yoshi-helpers/queries';
+
+const insight = new Insight({
+  trackingCode: 'UA-120893726-1',
+  // @ts-ignore
+  clientId: config.name,
+  packageName: 'yoshi',
+});
+
+const { version: yoshiVersion } = require('../package.json');
+
+const version = semver.parse(yoshiVersion)?.major;
+
+const isTypescriptProject = checkIsTypescriptProject();
+
+export async function collectData() {
+  insight.trackEvent({
+    category: 'version',
+    action: `${version}`,
+    label: config.name,
+  });
+
+  insight.trackEvent({
+    category: 'language',
+    action: isTypescriptProject ? 'ts' : 'js',
+    label: config.name,
+  });
+
+  // Since we call `process.exit()` directly we have to wait
+  return new Promise(resolve => setImmediate(resolve));
+}
--- packages/yoshi/bin/yoshi-cli.js
+++ packages/yoshi/bin/yoshi-cli.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
 const config = require('yoshi-config');
+const { inTeamCity } = require('yoshi-helpers/queries');
+const { collectData } = require('yoshi-common/build/telemetry');
 
-if (config.projectType === 'app') {
-  require('yoshi-flow-app/build/bin/yoshi-app');
-} else if (config.experimentalMonorepo) {
-  require('yoshi-flow-monorepo/build/bin/yoshi-monorepo');
-} else {
-  require('yoshi-flow-legacy/bin/yoshi-legacy');
-}
+const done = inTeamCity() ? collectData() : Promise.resolve();
+
+done.then(() => {
+  if (config.projectType === 'app') {
+    require('yoshi-flow-app/build/bin/yoshi-app');
+  } else if (config.experimentalMonorepo) {
+    require('yoshi-flow-monorepo/build/bin/yoshi-monorepo');
+  } else {
+    require('yoshi-flow-legacy/bin/yoshi-legacy');
+  }
+});
