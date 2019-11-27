a8751c95df1d381735960cd8002ca888560bc55d
Ronen Amiel <ronen.amiel@gmail.com>
2019-11-27 19:01:16 +0200

    Stop telemetry until we have our own custom server (#1730)

--- packages/yoshi/bin/yoshi-cli.js
+++ packages/yoshi/bin/yoshi-cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 const config = require('yoshi-config');
-const { inTeamCity } = require('yoshi-helpers/queries');
-const { collectData } = require('yoshi-common/telemetry');
+// const { inTeamCity } = require('yoshi-helpers/queries');
+// const { collectData } = require('yoshi-common/build/telemetry');
 
-const done = inTeamCity() ? collectData() : Promise.resolve();
+// const done = inTeamCity() ? collectData() : Promise.resolve();
 
-done.then(() => {
-  if (config.projectType === 'app') {
-    require('yoshi-flow-app/build/bin/yoshi-app');
-  } else if (config.experimentalMonorepo) {
-    require('yoshi-flow-monorepo/build/bin/yoshi-monorepo');
-  } else {
-    require('yoshi-flow-legacy/bin/yoshi-legacy');
-  }
-});
+// done.then(() => {
+if (config.projectType === 'app') {
+  require('yoshi-flow-app/build/bin/yoshi-app');
+} else if (config.experimentalMonorepo) {
+  require('yoshi-flow-monorepo/build/bin/yoshi-monorepo');
+} else {
+  require('yoshi-flow-legacy/bin/yoshi-legacy');
+}
+// });
