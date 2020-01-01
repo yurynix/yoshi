51904eb1eaea2dde33a2852651b03efbc3ea84db
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-01-01 14:40:00 +0200

    reporter

--- jest-ts.config.js
+++ jest-ts.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-puppeteer',
   rootDir: './test/typescript/features',
+  testResultsProcessor: 'jest-teamcity-reporter',
   testMatch: ['**/*.test.js'],
   globalSetup: require.resolve('./test/typescript/globalSetup'),
   globalTeardown: require.resolve('./test/typescript/globalTeardown'),
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-puppeteer',
   rootDir: './test/javascript/features',
+  testResultsProcessor: 'jest-teamcity-reporter',
   testMatch: ['**/*.test.js'],
   globalSetup: require.resolve('./test/javascript/globalSetup'),
   globalTeardown: require.resolve('./test/javascript/globalTeardown'),
