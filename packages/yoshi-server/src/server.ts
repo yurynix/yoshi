705a552c995d6804fb0d632cd0b62b342cd9291f
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-02-04 15:36:28 +0200

    yoshi-server: add global middleware support

--- packages/yoshi-server/src/server.ts
+++ packages/yoshi-server/src/server.ts
@@ -1,12 +1,14 @@
 import path from 'path';
 import { parse as parseUrl } from 'url';
 import { RequestHandler, Request, Response } from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { NextHandleFunction } from 'connect';
 import Youch from 'youch';
 import globby from 'globby';
 import SockJS from 'sockjs-client';
 import { send } from 'micro';
 import importFresh from 'import-fresh';
-import { ROUTES_BUILD_DIR } from 'yoshi-config/paths';
+import { ROUTES_BUILD_DIR, BUILD_DIR } from 'yoshi-config/paths';
 import { RouteFunction } from './types';
 import { relativeFilePath, pathMatch } from './utils';
 
@@ -22,9 +24,12 @@ export type Route = {
 export default class Server {
   private context: any;
   private routes: Array<Route>;
+  private globalMiddlewares: Array<NextHandleFunction>;
 
   constructor(context: any) {
     this.context = context;
+
+    this.globalMiddlewares = this.createGlobalMiddleware();
 
     this.routes = this.createRoutes();
 
@@ -36,6 +41,7 @@ export default class Server {
       socket.onmessage = async () => {
         try {
           this.routes = this.createRoutes();
+          this.globalMiddlewares = this.createGlobalMiddleware();
         } catch (error) {
           socket.send(JSON.stringify({ success: false }));
         }
@@ -45,8 +51,25 @@ export default class Server {
     }
   }
 
+  private async applyMiddlewares(
+    middleware: Array<NextHandleFunction>,
+    req: Request,
+    res: Response,
+  ) {
+    await middleware.reduce(async (acc, middleware): Promise<void> => {
+      await acc;
+      return new Promise((resolve, reject) => {
+        middleware(req, res, (error: any) =>
+          error ? reject(error) : resolve(),
+        );
+      });
+    }, Promise.resolve());
+  }
+
   public handle: RequestHandler = async (req, res): Promise<void> => {
     try {
+      await this.applyMiddlewares(this.globalMiddlewares, req, res);
+
       const { pathname } = parseUrl(req.url as string, true);
 
       for (const { handler, route } of this.routes) {
@@ -70,6 +93,12 @@ export default class Server {
     return send(res, 404, 'not found');
   };
 
+  private createGlobalMiddleware(): Array<NextHandleFunction> {
+    return importFresh(path.resolve(BUILD_DIR, '_middleware_.js')) as Array<
+      NextHandleFunction
+    >;
+  }
+
   private createRoutes(): Array<Route> {
     const routesBuildDir = path.resolve(ROUTES_BUILD_DIR);
 
@@ -83,6 +112,7 @@ export default class Server {
       const relativePath = `/${relativeFilePath(routesBuildDir, absolutePath)}`;
       // Change `/users/[userid]` to `/users/:userid`
       const routePath = relativePath.replace(/\[(\w+)\]/g, ':$1');
+
       return {
         route: routePath === '/index' ? '/' : routePath,
         handler: async (req, res, params) => {
