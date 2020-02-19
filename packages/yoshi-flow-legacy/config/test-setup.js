b459aaa57572439140afba17eeea1ce12ff3dd6b
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-02-19 19:19:27 +0200

    Yoshi Server: add support for mocha (#1981)

--- packages/yoshi-flow-legacy/config/test-setup.js
+++ packages/yoshi-flow-legacy/config/test-setup.js
@@ -7,10 +7,20 @@ const path = require('path');
 const { tryRequire } = require('yoshi-helpers/utils');
 const { setupRequireHooks } = require('yoshi-common/build/require-hooks');
 const { isTypescriptProject } = require('yoshi-helpers/build/queries');
+const projectConfig = require('yoshi-config');
+const { addHook } = require('pirates');
+const { transform } = require('yoshi-server-tools/build/utils');
 
 const ext = isTypescriptProject() && !process.env.IN_WALLABY ? 'ts' : 'js';
 const mochaSetupPath = path.join(process.cwd(), 'test', `mocha-setup.${ext}`);
 const setupPath = path.join(process.cwd(), 'test', `setup.${ext}`);
+
+if (projectConfig.yoshiServer) {
+  addHook(transform, {
+    exts: ['.js'],
+    matcher: filename => filename.endsWith('.api.js'),
+  });
+}
 
 if (!process.env.IN_WALLABY) {
   setupRequireHooks();
