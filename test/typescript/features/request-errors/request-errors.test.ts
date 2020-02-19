55f6f6c48e7aad5c04dd53424872522841232956
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-02-19 22:47:27 +0200

    Increase timeout for flaky TS test (#1985)

--- test/typescript/features/request-errors/request-errors.test.ts
+++ test/typescript/features/request-errors/request-errors.test.ts
@@ -5,6 +5,8 @@ const scripts = Scripts.setupProjectFromTemplate({
   projectType: 'typescript',
 });
 
+jest.setTimeout(35000);
+
 describe.each(['prod', 'dev'] as const)('e2e [%s]', mode => {
   it('run tests', async () => {
     await scripts[mode](async () => {
