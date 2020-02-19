08fcd6af91f2d3d8a102915f69712bb517123317
Artem Yavorsky <info@yavorsky.org>
2020-02-19 09:31:26 +0200

    fix(create-yoshi-app): Increase timeout time for h2 selector for viewer. (#1976)

--- packages/create-yoshi-app/templates/out-of-iframe/javascript/src/viewerApp/viewerApp.e2e.js
+++ packages/create-yoshi-app/templates/out-of-iframe/javascript/src/viewerApp/viewerApp.e2e.js
@@ -3,7 +3,9 @@ const { viewerUrl } = require('../../dev/sites');
 describe('Viewer App', () => {
   it('should display the title text', async () => {
     await page.goto(viewerUrl);
-    await page.waitForSelector('h2');
+    await page.waitForSelector('h2', {
+      timeout: 15000,
+    });
 
     expect(await page.$eval('h2', e => e.textContent)).toEqual('Hello World!');
   });
