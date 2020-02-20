0762a30c10ad8cba2c3b1baf6f45a183727d9da7
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-02-20 22:01:43 +0200

    Yoshi Server Testing: better error messages (#1988)

--- packages/yoshi-server-testing/src/index.ts
+++ packages/yoshi-server-testing/src/index.ts
@@ -19,7 +19,7 @@ type Mock<Result extends FunctionResult, Args extends FunctionArgs> = {
 export default class implements HttpClient {
   private mocks: Array<Mock<any, any>>;
 
-  constructor(mocks: Array<Mock<any, any>>) {
+  constructor(mocks: Array<Mock<any, any>> = []) {
     this.mocks = mocks;
   }
 
@@ -27,6 +27,15 @@ export default class implements HttpClient {
     dsl: DSL<Result, Args>,
     ...args: Args
   ): Promise<UnpackPromise<Result>> {
+    if (!this.mocks.length) {
+      throw new Error(
+        `\n\n
+        A request to ${JSON.stringify(dsl)} was made without supplying a mock.
+        Please supply an array of mocks for each request you make.
+        `,
+      );
+    }
+
     const mock = this.mocks.find(({ request }) => {
       if (request.fn === dsl) {
         return isEqual(args, request.variables);
@@ -37,6 +46,15 @@ export default class implements HttpClient {
       return mock.result();
     }
 
-    throw new Error('not found');
+    throw new Error(
+      `\n\n
+      We couldn't find a mock for the following request: ${JSON.stringify(
+        dsl,
+      )}, with arguments: ${args}.
+      The array of mocks is:
+      ${JSON.stringify(this.mocks)}
+      Please verify that you supplied the correct array of mocks.
+      `,
+    );
   }
 }
