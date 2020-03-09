de913d4835ff6d73d7d3067451a62c714999288d
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2020-03-09 15:48:58 +0200

    Deprecate `yoshi release` command (#2069)

--- packages/yoshi-flow-legacy/src/commands/release.js
+++ packages/yoshi-flow-legacy/src/commands/release.js
@@ -2,6 +2,7 @@ const wnpm = require('wnpm-ci');
 const { createRunner } = require('haste-core');
 const LoggerPlugin = require('../plugins/haste-plugin-yoshi-logger');
 const parseArgs = require('minimist');
+const boxen = require('boxen');
 
 const cliArgs = parseArgs(process.argv.slice(2));
 
@@ -11,6 +12,29 @@ const runner = createRunner({
   logger: new LoggerPlugin(),
 });
 
-module.exports = runner.command(async () =>
-  wnpm.prepareForRelease({ shouldBumpMinor }),
-);
+module.exports = runner.command(async () => {
+  console.warn(
+    boxen(
+      `
+The command \`yoshi release\` is DEPRECATED
+and will be removed in the next major version (yoshi@5)
+
+In order to continue and bump the minor/patch version in the CI,
+please add this to the \`publishConfig\` in \`package.json\`
+
+...
+  "publishConfig": {
+    "registry": "http://npm.dev.wixpress.com/",
+    "versionBumpStrategy": "minor"
+  },
+...
+
+Note that this is a CI feature, for more information see:
+
+https://github.com/wix-private/wix-fed-scripts/pull/37
+  `,
+      { padding: 1 },
+    ),
+  );
+  wnpm.prepareForRelease({ shouldBumpMinor });
+});
