bb9d9e88bdc3395a0b24db42b819a921389627a5
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-03-22 09:23:43 +0200

    Yoshi Server: add a test for initData on routes (#2115)

--- test/javascript/features/yoshi-server/init-server/init-server.test.ts
+++ test/javascript/features/yoshi-server/init-server/init-server.test.ts
@@ -6,9 +6,9 @@ const scripts = Scripts.setupProjectFromTemplate({
 });
 
 describe.each(['prod', 'dev'] as const)(
-  'yoshi-server api client to server [%s]',
+  'yoshi-server init data, api [%s]',
   mode => {
-    it('run tests', async () => {
+    it('title should contain initData', async () => {
       await scripts[mode](
         async () => {
           await page.goto(`${scripts.serverUrl}/app`);
@@ -17,6 +17,25 @@ describe.each(['prod', 'dev'] as const)(
           );
           const title = await page.$eval('h2', elm => elm.innerHTML);
           expect(title).toBe('hello Yaniv FOO');
+        },
+        { env: { YOSHI_SERVER_TEST: 'FOO' } },
+      );
+    });
+  },
+);
+
+describe.each(['prod', 'dev'] as const)(
+  'yoshi-server init data, route [%s]',
+  mode => {
+    it('title should have initData value', async () => {
+      await scripts[mode](
+        async () => {
+          await page.goto(`${scripts.serverUrl}/app`);
+          await page.waitForFunction(
+            `document.getElementById('my-text').innerText !== ''`,
+          );
+          const title = await page.$eval('title', elm => elm.innerHTML);
+          expect(title).toBe('FOO');
         },
         { env: { YOSHI_SERVER_TEST: 'FOO' } },
       );
--- test/javascript/features/yoshi-server/init-server/src/routes/app.js
+++ test/javascript/features/yoshi-server/init-server/src/routes/app.js
@@ -1,7 +1,7 @@
 import { route, render } from 'yoshi-server';
 
 export default route(async function() {
-  const html = await render('app');
+  const html = await render('app', { title: this.initData });
 
   return html;
 });
