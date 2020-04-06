f3c1a71247a849a71d6be789f08957ebc0a0c3ff
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-04-06 14:42:32 +0300

    Yoshi Server: refactor InternalServerError (#2161)

--- packages/yoshi-server/src/httpErrors.ts
+++ packages/yoshi-server/src/httpErrors.ts
@@ -1,8 +1,9 @@
-import { wixBusinessError, HttpStatus } from '@wix/wix-errors';
+import { WixBusinessError, HttpStatus } from '@wix/wix-errors';
 
-export class InternalServerError extends wixBusinessError(
-  1011,
-  HttpStatus.INTERNAL,
-) {
+export class InternalServerError extends WixBusinessError {
   // default error code: -100, default http status: 500.
+  // See https://github.com/wix-platform/wix-node-platform/blob/master/errors/wix-errors/README.md
+  constructor(msg?: string, cause?: Error | string) {
+    super(msg, { cause, errorCode: 1011, httpStatusCode: HttpStatus.INTERNAL });
+  }
 }
