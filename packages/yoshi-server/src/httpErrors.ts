ac681b8d15a49c8600d73dfd5b7cbe508d3472ad
wix-renovate <60687886+wix-renovate@users.noreply.github.com>
2020-04-12 19:44:33 +0300

    Lock file maintenance (#2108)
    * Lock file maintenance
    
    * Bump @types/react
    
    * Bump @types/webpack
    
    * Manual lockfile refresh
    
    * Refresh lockfile again
    
    * Add babel-loader to storybook deps
    
    * Manual lockfile refresh
    
    * Bump @types/react & @wix/wix-experiments
    
    * Fix @wix/wix-errors breaking change
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Amit Dahan <amitda@wix.com>

--- packages/yoshi-server/src/httpErrors.ts
+++ packages/yoshi-server/src/httpErrors.ts
@@ -4,6 +4,10 @@ export class InternalServerError extends WixBusinessError {
   // default error code: -100, default http status: 500.
   // See https://github.com/wix-platform/wix-node-platform/blob/master/errors/wix-errors/README.md
   constructor(msg?: string, cause?: Error | string) {
-    super(msg, { cause, errorCode: 1011, httpStatusCode: HttpStatus.INTERNAL });
+    super(msg, {
+      cause,
+      errorCode: 1011,
+      httpStatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+    });
   }
 }
